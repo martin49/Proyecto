e02c7acb1f3558fcb73f9247d8eacdd7df0b09ab
Martin Pastora <MartinPastora@MacBook-Pro-de-Martin.local>
2014-10-25 20:39:37 -0600

    Cambios en la gramatica
    Se agregaron nuevas funciones a la gramática

--- proyecto.pas
+++ proyecto.pas
@@ -1,11 +1,22 @@
-program d
-var a,b:string 10]
-n:Array[1..4]  Integer;
+program d;
+var a,b:Integer;
+procedure h;
+	procedure x
+		begin
+			j:=6+5;
+		end.	
 begin
-  a := '1234567890'
+	d:=3;
+end.
+function p : Integer;
+begin
+	a:=b+3;
+end.
+begin
+  a := '1234567890';
   for n := 1 to 10 do
-      write('hola',a)
+      write('hola',a);
   a := 'The quick brown fox';
   b := 'the lazy dog.';
  a(a);
-end
+end.
