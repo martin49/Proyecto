11ea90663369928f6cb53e73871e22f62b649bc4
Martin Pastora <MartinPastora@MacBook-Pro-de-Martin.local>
2014-11-05 07:01:52 -0600

    Comprobacion de Tipos
    Agregar los tipos de las variables y el ámbito a la tabla de símbolos

--- proyecto.pas
+++ proyecto.pas
@@ -1,30 +1,7 @@
 program d;
-var a:Integer;
-procedure h;
-	var B:Integer;
-	procedure x;
-		var C:Integer;
-		begin
-			if a > 6 then
-				begin
-					a:=3;
-				end;
-			else
-				a:=6;
-		end.	
+var a,g,t:char;
+	b,c:Integer;
 begin
-	d:=3;
-end.
-function p : Integer;
-var d:Integer;
-begin
-	a:=b+3;
-end.
-begin
-  a := a(a);
-  for n := 1 to 10 do
-      write('hola',a);
-  a := 'The quick brown fox';
-  b := 'the lazy dog.';
- a(a);
+  a := 'v';
+  b := g;
 end.
---
+++ src/tabla.java
@@ -0,0 +1,91 @@
+import java.util.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+class Simbolo
+{  
+    String nombre;
+    String tipo;
+    String ambito;
+    
+    public Simbolo(String nombre, String tipo, String ambito) 
+    {
+        this.nombre = nombre;       
+        this.tipo = tipo;
+        this.ambito = ambito;
+    }
+    
+}
+
+class TablaSimbolos {
+    static Map<String, Simbolo> tablaSimbolos;
+    ArrayList<String> sim;
+    public static Logger log = Logger.getLogger(TablaSimbolos.class.getName());            
+    
+    public TablaSimbolos()
+    {        
+        tablaSimbolos = new HashMap<String, Simbolo>();  
+        sim = new ArrayList<String>();
+    }
+    
+            
+    public Simbolo crear(String nombre, String tipo, String ambito)
+    {        
+        Simbolo simbolo = buscar(nombre);                            
+        if(simbolo == null) // La variable no existe
+        {
+            simbolo = new Simbolo(nombre, tipo, ambito);
+            System.out.println("Agregando a tabla de simbolos con nombre: " + nombre);
+            tablaSimbolos.put(nombre, simbolo);            
+            //System.out.println("Variable creada exitosamente!!!");
+              
+            imprimir();                
+            System.out.println(" ");
+            return simbolo;
+        }
+        else
+        {
+            log.log(Level.SEVERE, "Redefinición de la variable: " + nombre);
+            return null; // La variable ya existía                
+        }
+    }
+    
+    public void creartipo(String tipo)
+    {        
+      if(sim.isEmpty()){
+         sim.add(tipo);
+      }else{
+          sim.remove(0);
+          sim.add(tipo);
+      }  
+        
+    }
+    
+    public String getTipo(){
+        return sim.get(0);
+    }
+   
+        
+    
+    public Simbolo buscar(String nombre)
+    {
+        return (Simbolo)tablaSimbolos.get(nombre);
+    }
+    
+    public String buscarTipo(String nombre)
+    {
+        return tablaSimbolos.get(nombre).tipo;
+    }
+    
+    public void imprimir()
+    {
+        System.out.println("\nIngresando a imprimir de TablaSimbolos");
+        System.out.println("    Valores de la tabla de simbolos:");
+        for (Simbolo s : tablaSimbolos.values())
+            System.out.println(String.format("      "
+                    + "Nombre: %s, tipo: %s, ambito: %s",s.nombre, s.tipo, s.ambito));        
+        System.out.println("Saliendo de imprimir en TablaSimbolos\n ");        
+    }
+    
+}
