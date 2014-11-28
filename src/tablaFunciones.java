6baac7f9dd5a63ec0fc51b1ca146ddc67a59e1f7
Martin Pastora <MartinPastora@MacBook-Pro-de-Martin.local>
2014-11-28 00:46:49 -0600

    gg
    fgg

---
+++ src/tablaFunciones.java
@@ -0,0 +1,32 @@
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author MartinPastora
+ */
+public class tablaFunciones {
+    String nombre;
+    String tipo;
+    String ambito;
+    
+    public tablaFunciones(String nombre, String tipo, String ambito) 
+    {
+        this.nombre = nombre;       
+        this.tipo = tipo;
+        this.ambito = ambito;
+    }
+    
+}
+
+    
+
