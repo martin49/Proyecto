59570cc5b9c2424920bb481a68a8f480c98c98e2
Martin Pastora <MartinPastora@MacBook-Pro-de-Martin.local>
2014-12-02 22:19:07 -0600

    Validacion String
    Ya se valida si se puede asignar string

--- proyecto.pas
+++ proyecto.pas
@@ -2,9 +2,33 @@ program d;
 var a,g,t:char;
 	b,c:Integer;
 	h,f:Array[1..4] of Integer;
-	m: string[10];
-
+	m: string[100];
+	procedure Q(gsd:Integer; fdas:char;);
+		var B,k:Integer;
+		procedure x;
+			var C,j,l:Integer;
+			begin
+				j :=3;
+				b := c+4;
+			end.
+		procedure L;
+			var ddsa:Integer;
+			begin
+				ddsa := 3;
+			end.		
+		begin
+			t:='r';
+		end.
+	function p(zx,ddf,ADS,FRE:Integer; asde:char;) : Integer;
+		var d,s,z:char;
+		begin
+			a:=s;
+			b:=c*56;
+		end.
 begin
-  b := 4;
+  a := g;
+  b := h[3];
   b := (c*4);
+  m := 'holhhhhhhhhhhhhh'; 
 end.
+
